3af4efb80538fd30a3ab43e724058d87ca3b3f07
Ankitpardeshiii <ankitpardeshi2025@gmail.com>
2026-04-15 12:17:18 +0530

    update: improve homepage UI

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function toggleCode(id) {
-    const el = document.getElementById(id);
-    el.style.display = el.style.display === "block" ? "none" : "block";
-  }
-  
-  function copyCode(id) {
-    const code = document.getElementById(id).innerText;
-  
-    navigator.clipboard.writeText(code)
-      .then(() => alert("Copied! 🚀"))
-      .catch(() => alert("Error copying"));
-  }
+  const el = document.getElementById(id);
+  el.style.display = el.style.display === "block" ? "none" : "block";
+}
+
+function copyCode(id) {
+  const code = document.getElementById(id).innerText;
+
+  navigator.clipboard.writeText(code)
+    .then(() => alert("Copied! 🚀"))
+    .catch(() => alert("Error copying"));
+} 
